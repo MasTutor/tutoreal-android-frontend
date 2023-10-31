07de066c853b16fb412762dc982010eed43d6cab
HouseofFoam <ifanperdana55@gmail.com>
2023-11-01 01:10:16 +0700

    Tambah Tutor Components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    implementation("androidx.compose.ui:ui-text-google-fonts:1.5.4")
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
--- app/src/main/java/com/mastutor/tutoreal/MainActivity.kt
+++ app/src/main/java/com/mastutor/tutoreal/MainActivity.kt
@@ -3,13 +3,21 @@ package com.mastutor.tutoreal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mastutor.tutoreal.ui.components.MatchTutorComponent
+import com.mastutor.tutoreal.ui.components.TutorComponent
 import com.mastutor.tutoreal.ui.theme.TutorealTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +25,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TutorealTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Anjing")
+                Column(modifier = Modifier.background(color = MaterialTheme.colorScheme.secondary)) {
+                    TutorComponent(
+                        photoUrl = "https://images.pexels.com/photos/1674666/pexels-photo-1674666.jpeg",
+                        name = "Jim Burton",
+                        job = "Con Artist",
+                        price = 200000,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(160.dp)
+                            .padding(20.dp)
+                    )
+                    MatchTutorComponent(
+                        photoUrl = "https://images.pexels.com/photos/1674666/pexels-photo-1674666.jpeg",
+                        name = "Jim Burton",
+                        job = "Con Artist",
+                        price = 200000,
+                        percentage = 98.0,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(160.dp)
+                            .padding(10.dp)
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TutorealTheme {
-        Greeting("Android")
     }
 }
