33b43bd3ff28dc413e4c573bcb7e95600d03c102
HouseofFoam <ifanperdana55@gmail.com>
2023-11-13 00:32:15 +0700

    Code Cleanup

--- app/src/main/java/com/mastutor/tutoreal/MainActivity.kt
+++ app/src/main/java/com/mastutor/tutoreal/MainActivity.kt
@@ -3,10 +3,6 @@ package com.mastutor.tutoreal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import com.mastutor.tutoreal.data.local.QuestionData
-import com.mastutor.tutoreal.ui.screen.survey.SurveyContent
 import com.mastutor.tutoreal.ui.theme.TutorealTheme
 
 class MainActivity : ComponentActivity() {
