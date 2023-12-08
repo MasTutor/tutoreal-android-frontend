e8d2d44e0c8983b4231b2a25cf8076764df78151
southerntw <fauzanmansur.a@gmail.com>
2023-12-09 01:09:07 +0800

    Fix: Solve BBB (Big Black Bar) Incident

--- app/src/main/java/com/mastutor/tutoreal/MainActivity.kt
+++ app/src/main/java/com/mastutor/tutoreal/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.mastutor.tutoreal.ui.theme.TutorealTheme
@@ -15,13 +18,13 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.light(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT),
             navigationBarStyle = SystemBarStyle.light(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT)
         )
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
-        installSplashScreen()
         setContent {
             TutorealTheme {
                  MainJetpack(modifier = Modifier.fillMaxSize())
